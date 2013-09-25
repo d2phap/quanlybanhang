5ceb25ad011360055bb65e7edc3ef8dcf742f99a
Dương Diệu Pháp <phap@imageglass.org>
2013-09-25 22:01:49 +0700

    v1 Design HTML
    - Log in
    - Quản lý danh mục

---
+++ design/files/js/danhmuc.js
@@ -0,0 +1,77 @@
+//Cho phép edit textbox
+function editableField(id)
+{
+	$(id).prop("readonly", false);
+	$(id).addClass("editField-Focus");
+}
+
+//Không cho phép edit textbox
+function readOnlyField(id)
+{
+	$(id).prop("readonly", true);
+	$(id).removeClass("editField-Focus");
+}
+
+//Thay đổi trạng thái của item
+function changeStatus(id, statusCode)
+{
+	if(statusCode == 1) //Kich hoat
+	{
+		$(id).prop("src", "files/images/check.png");
+		$(id).prop("title", "Kích hoạt");
+	}
+	else //An
+	{
+		$(id).prop("src", "files/images/delete.png");
+		$(id).prop("title", "Ẩn");
+	}
+}
+
+//Xoá danh mục
+function deleteItem(id)
+{
+	var itemID = parseInt($(id).attr("data-id"));
+	
+	if(confirm("Bạn có chắc muốn xoá dòng có ID = " + itemID + " ?"))
+	{
+		//Xoá danh mục
+		//
+	}
+}
+
+
+$(document).ready(function() {
+	$(".editField").dblclick(function(){
+		editableField(this);
+	});
+	
+	$(".editField").keyup(function(e) {
+		//ENTER || ESC
+        if((!e.altKey && !e.ctrlKey && !e.shiftKey) && (e.keyCode == 13 || e.keyCode == 27))
+		{
+			readOnlyField(this);
+		}
+	});
+	
+	$(".imgTrangThai").click(function(){
+		var statusCode = parseInt($(this).attr("data-code"));
+		
+		if(statusCode == 1)
+		{
+			changeStatus(this, 0);
+			$(this).attr("data-code", 0);
+		}
+		else
+		{
+			changeStatus(this, 1);
+			$(this).attr("data-code", 1);
+		}
+		
+	});
+	
+	$(".lnkXoaDanhMuc").click(function() {
+        deleteItem(this);
+    });
+	
+});
+
